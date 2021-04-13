52954bca371841b6ff70f00d179a8db9efae4f43
jwu0 <jackiewu0@gmail.com>
2021-04-13 09:33:13 -1000

    basic setup

---
+++ app/imports/ui/pages/ProfilePage.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Container, Card, Header, Loader, Image, Icon } from 'semantic-ui-react';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import { Stuffs } from '../../api/stuff/Stuff';
+import StuffItem from '../components/StuffItem';
+
+/** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+class ProfilePage extends React.Component {
+
+  // If the subscription(s) have been received, render the page, otherwise show a loading icon.
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  // Render the page once subscriptions have been received.
+  renderPage() {
+    return (
+      <Container>
+        <Header as="h2" textAlign="center">Student Profiles</Header>
+        <Card>
+          <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>Matthew</Card.Header>
+            <Card.Meta>
+              <span className='date'>Joined in 2015</span>
+            </Card.Meta>
+            <Card.Description>
+              Matthew is a musician living in Nashville.
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              22 Friends
+            </a>
+          </Card.Content>
+        </Card>
+      </Container>
+    );
+  }
+}
+
+// Require an array of Stuff documents in the props.
+ProfilePage.propTypes = {
+  stuffs: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+// withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe(Stuffs.userPublicationName);
+  // Determine if the subscription is ready
+  const ready = subscription.ready();
+  // Get the Stuff documents
+  const stuffs = Stuffs.collection.find({}).fetch();
+  return {
+    stuffs,
+    ready,
+  };
+})(ProfilePage);
