562421eda49ed981c98cfe887475a90c6428fad1
jhonda7 <jhonda7@users.noreply.github.com>
2021-04-13 18:06:09 -1000

    Fixed footer text color

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -5,15 +5,16 @@ import { Link } from 'react-router-dom';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
+    const greenText = { color: '#024731' };
     return (
       <footer className='footer-fixation'>
         <div>
           <Button.Group widths='2'>
             <Button>
-              <a href="https://bowed-in.github.io">About BowedIn</a>
+              <a href="https://bowed-in.github.io" style={greenText}>About BowedIn</a>
             </Button>
             <Button>
-              <Link to="/signup">About the BowedIn Team</Link>
+              <Link to="/signup" style={greenText}>About the BowedIn Team</Link>
             </Button>
           </Button.Group>
         </div>
