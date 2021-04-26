15329942092c633da2a616200cd12959de13a3c6
jhonda7 <jhonda7@users.noreply.github.com>
2021-04-25 16:01:32 -1000

    Added UI Buttons and Routing  in TopLanding

--- app/imports/ui/components/TopLanding.jsx
+++ app/imports/ui/components/TopLanding.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import { Grid, Header, Container } from 'semantic-ui-react';
+import { Grid, Header, Container, Button } from 'semantic-ui-react';
+import { Redirect } from 'react-router-dom';
 
 export default class TopLanding extends React.Component {
+  constructor(props) {
+    super(props);
+    /* NOTE: change this.state = { email, choice: ' ', ... } Double check the dropdown field settings */
+    this.state = { redirectTo: undefined };
+  }
+
+  submitSignin = () => {
+    this.setState({ redirectTo: '/signin' });
+  }
+
+  submitSignup = () => {
+    this.setState({ redirectTo: '/signup' });
+  }
+
   render() {
+    if (this.state.redirectTo) {
+      return <Redirect to={ this.state.redirectTo }/>;
+    }
     const gridStyle = { height: '500px', fontSize: '75px' };
     return (
       <div className="top-landing-background">
@@ -13,6 +31,14 @@ export default class TopLanding extends React.Component {
                 <Header inverted>
                   It&apos;s time to get BowedIn!
                 </Header>
+                <Button onClick={this.submitSignin} circular animated='fade' color='orange'>
+                  <Button.Content visible>I already have an account</Button.Content>
+                  <Button.Content hidden>Sign in to your account!</Button.Content>
+                </Button>
+                <Button onClick={this.submitSignup} circular animated='fade' color='orange'>
+                  <Button.Content visible>Uh oh, I don&apos;t have an account yet!</Button.Content>
+                  <Button.Content hidden>Sign up for a free account!</Button.Content>
+                </Button>
               </Container>
             </Grid.Column>
           </Grid.Row>
