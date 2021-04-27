258b0ce512d1daabf16e813f756fa7ce97cdddcd
Justin Loi <jmloi@hawaii.edu>
2021-04-27 08:01:43 -1000

    Base of Company Home Page

--- app/imports/ui/components/PotentialHire.jsx
+++ app/imports/ui/components/PotentialHire.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
-import { Image, Item } from 'semantic-ui-react';
+import { Button, Card, Image, Item } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class PotentialHire extends React.Component {
   render() {
-    const font = { color: 'white' };
     return (
-      <Item>
-        <Image size='small' src={this.props.potentialHire.image} />
-        <Item.Content>
-          <Item.Header style={font} as='a'>{this.props.potentialHire.firstName} {this.props.potentialHire.lastName}</Item.Header>
-          <Item.Extra style={font}>Skills: {this.props.potentialHire.skill}</Item.Extra>
-          <Item.Extra style={font}>Interests: {this.props.potentialHire.interest}</Item.Extra>
-        </Item.Content>
-      </Item>
+      <Card>
+        <Card.Content>
+          <Image
+            floated='left'
+            size='tiny'
+            src={this.props.potentialHire.image}
+          />
+          <Card.Header textAlign="left">{this.props.potentialHire.firstName} {this.props.potentialHire.lastName}</Card.Header>
+          <Card.Content extra textAlign={'left'}>
+            <b> Skills: </b> {this.props.potentialHire.skill}
+          </Card.Content>
+          <Card.Content extra textAlign={'left'}>
+            <b> Interests: </b> {this.props.potentialHire.interest}
+          </Card.Content>
+        </Card.Content>
+        <Card.Content extra>
+          <div className='ui two buttons'>
+            <Button basic color='red'>
+                View Profile
+            </Button>
+            <Button basic color='blue'>
+                Message
+            </Button>
+          </div>
+        </Card.Content>
+      </Card>
     );
   }
 }
